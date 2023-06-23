220712eea9a43dd582e60fe9d6dc4e272b740bce
nzone56 <jpereirap@unal.edu.co>
2023-06-22 20:30:19 -0500

    Introduction DONE

---
+++ src/pages/FirstSeason/Introduction.jsx
@@ -0,0 +1,188 @@
+import { Box, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+
+export const Introduction = () => {
+   // TODO: MIXIN OR SMTH TO GROPU AND RE-USE ANIMATION CSS
+   const [scrollDistance, setScrollDistance] = useState(0)
+   const triggerDistance = 500
+
+   useEffect(() => {
+      const handleScroll = () => {
+         const scrollTop = window.scrollY || document.documentElement.scrollTop
+         setScrollDistance(scrollTop)
+      }
+
+      window.addEventListener('scroll', handleScroll)
+
+      return () => {
+         window.removeEventListener('scroll', handleScroll)
+      }
+   }, [])
+
+   const transformValue = scrollDistance ? `-${scrollDistance / 5}px` : '0px'
+   const isAnimated = scrollDistance >= triggerDistance
+   return (
+      <Box
+         id="Introduction"
+         component="article"
+         sx={{
+            position: 'relative',
+            padding: '8.5rem 0',
+            backgroundImage: `
+             url(../../src/assets/FirstSeason/Introduction/bg_box.png), 
+      url(../../src/assets/FirstSeason/Introduction/bg_pink-line_lt.png),
+      url(../../src/assets/FirstSeason/Introduction/bg_pink-line_rb.png),
+          
+      linear-gradient(#000, #000)`,
+
+            backgroundPosition: 'center center, top left, bottom right',
+            backgroundSize: 'cover, 364px 332px, 832px 170px',
+            backgroundRepeat: 'no-repeat',
+         }}
+      >
+         {/* FLOATING TRIANGLE 1  */}
+         <Box
+            sx={{
+               position: 'absolute',
+               top: '210px',
+               left: '-300px',
+               width: '416px',
+               overflow: 'hidden',
+               transition:
+                  'transform 5s cubic-bezier(0.19, 1, 0.22, 1), -webkit-transform 5s cubic-bezier(0.19, 1, 0.22, 1)',
+               transform: `translateY(${transformValue})`,
+            }}
+         >
+            <img
+               src="../../src/assets/FirstSeason/Introduction/triangle0001.png"
+               alt="Floating triangle"
+               style={{
+                  width: '100%',
+                  maxWidth: '100%',
+                  height: 'auto',
+                  display: 'block',
+               }}
+            />
+         </Box>
+         {/* FLOATING TRIANGLE 2  */}
+         <Box
+            sx={{
+               position: 'absolute',
+               bottom: '-470px',
+               right: '-60px',
+               width: '416px',
+               overflow: 'hidden',
+               zIndex: '5',
+               transition:
+                  'transform 5s cubic-bezier(0.19, 1, 0.22, 1), -webkit-transform 5s cubic-bezier(0.19, 1, 0.22, 1)',
+               transform: `translateY(${transformValue})`,
+            }}
+         >
+            <img
+               src="../../src/assets/FirstSeason/Introduction/triangle0010.png"
+               alt="Floating triangle"
+               style={{
+                  width: '100%',
+                  maxWidth: '100%',
+                  height: 'auto',
+                  display: 'block',
+               }}
+            />
+         </Box>
+         {/* INTRODUCTION TITTLE */}
+         <Typography
+            component="h2"
+            variant="h2"
+            sx={{
+               display: 'block',
+
+               position: 'relative',
+               width: '39rem',
+               transform: isAnimated
+                  ? 'translateX(0) translateY(0)'
+                  : 'translateX(-10px)',
+               transition: 'all 0.5s cubic-bezier(0.645, 0.045, 0.355, 0)',
+               opacity: isAnimated ? '1' : '0',
+            }}
+         >
+            {' '}
+            <img
+               src="../../src/assets/FirstSeason/Introduction/title_intro.png"
+               alt="Logo de la empresa"
+               style={{
+                  width: '100%',
+                  maxWidth: '100%',
+                  height: 'auto',
+                  display: 'block',
+               }}
+            />
+         </Typography>
+         {/* INTRODUCTION DESCRIPTION  */}
+         <Box
+            sx={{
+               position: 'relative',
+               width: '68rem',
+               margin: '0 auto ',
+               padding: '45px 0 90px ',
+            }}
+         >
+            <Box
+               sx={{
+                  position: 'absolute',
+                  top: '-100px',
+                  right: '-180px',
+                  width: '102px',
+                  background: `url("../../src/assets/FirstSeason/Introduction/bg_triangle01-2.png")`,
+                  height: '80px',
+                  transform: isAnimated
+                     ? 'translateX(0) translateY(0)'
+                     : 'translateX(-10px)',
+                  transition: 'all 0.5s cubic-bezier(0.645, 0.045, 0.355, 0)',
+                  opacity: isAnimated ? '1' : '0',
+               }}
+            />
+            <Box
+               sx={{
+                  transform: isAnimated
+                     ? 'translateX(0) translateY(0)'
+                     : 'translateX(-10px)',
+                  transition: 'all 0.5s cubic-bezier(0.645, 0.045, 0.355, 0)',
+                  opacity: isAnimated ? '1' : '0',
+               }}
+            >
+               <Typography
+                  component="p"
+                  variant="body1"
+                  sx={{ width: '68rem', margin: '0 auto' }}
+               >
+                  <img
+                     src="../../src/assets/FirstSeason/Introduction/text_intro.png"
+                     alt="Logo de la empresa"
+                     style={{
+                        width: '100%',
+                        maxWidth: '100%',
+                        height: 'auto',
+                        display: 'block',
+                     }}
+                  />
+               </Typography>
+            </Box>
+            <Box
+               sx={{
+                  position: 'absolute',
+                  bottom: '80px',
+                  left: '-180px',
+                  width: '66px',
+                  background: `url("../../src/assets/FirstSeason/Introduction/bg_triangle02-2.png")`,
+                  height: '52px',
+                  transform: isAnimated
+                     ? 'translateX(0) translateY(0)'
+                     : 'translateX(-10px)',
+                  transition: 'all 0.5s cubic-bezier(0.645, 0.045, 0.355, 0)',
+                  opacity: isAnimated ? '1' : '0',
+               }}
+            />
+         </Box>
+      </Box>
+   )
+}
--- src/pages/FirstSeason/Main.jsx
+++ src/pages/FirstSeason/Main.jsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material'
 import { BubbleArea } from './BubbleArea'
 import { NewsPreview } from './NewsPreview'
 import { useEffect, useState } from 'react'
+import { Introduction } from './Introduction'
 
 export const Main = () => {
    const [scrollDistance, setScrollDistance] = useState(0)
@@ -30,44 +31,73 @@ export const Main = () => {
             overflow: 'hidden',
          }}
       >
+         {/* MAIN ARTICLE BACKGROUND WITH POP UPS  */}
          <Box
             component="article"
             sx={{
                position: 'relative',
-               width: '100%',
-               minHeight: '980px',
-               overflow: 'hidden',
+               display: 'block',
             }}
          >
-            {/* FONDO PRINCIPAL  */}
+            {/* MAIN SCENE */}
             <Box
                sx={{
-                  position: 'absolute',
-                  top: '-153.6px',
-                  left: '0',
+                  position: 'relative',
                   width: '100%',
-                  height: '300%',
-                  backgroundImage:
-                     'url(../../src/assets/FirstSeason/img_main.jpg)',
-                  backgroundSize: '100% auto',
-                  backgroundRepeat: 'no-repeat',
-                  transition: 'transform .7s ease-out, top 1.5s ease-out .7s',
-                  transform: `translateY(${transformValue})`,
-                  //TODO: ANIMATION FROM SCROLLING DOWN SLOW DOWN, MORE HEIGHT
-               }}
-            />
-            {/* POP UP SECOND SEASON  */}
-            <Box
-               sx={{
-                  maxWidth: '39rem',
-                  position: 'absolute',
-                  top: '1rem',
-                  right: '1rem',
+                  minHeight: '980px',
+                  overflow: 'hidden',
                }}
             >
-               <a href="/2nd">
+               {/* MAIN BACKGROUND   */}
+               <Box
+                  sx={{
+                     position: 'absolute',
+                     top: '-153.6px',
+                     left: '0',
+                     width: '100%',
+                     height: '300%',
+                     backgroundImage:
+                        'url(../../src/assets/FirstSeason/img_main.jpg)',
+                     backgroundSize: '100% auto',
+                     backgroundRepeat: 'no-repeat',
+                     transition:
+                        'transform .7s ease-out, top 1.5s ease-out .7s',
+                     transform: `translateY(${transformValue})`,
+                     //TODO: ANIMATION FROM SCROLLING DOWN SLOW DOWN, MORE HEIGHT
+                  }}
+               />
+               {/* POP UP SECOND SEASON  */}
+               <Box
+                  sx={{
+                     maxWidth: '39rem',
+                     position: 'absolute',
+                     top: '1rem',
+                     right: '1rem',
+                  }}
+               >
+                  <a href="/2nd">
+                     <img
+                        src="../../src/assets/FirstSeason/bnr_second.jpg"
+                        alt="Logo de la empresa"
+                        style={{
+                           width: '100%',
+                           maxWidth: '100%',
+                           height: 'auto',
+                           display: 'block',
+                        }}
+                     />
+                  </a>
+               </Box>
+               {/* POP UP LOGO DIAGONAL  */}
+               <Box
+                  sx={{
+                     maxWidth: '80.2rem',
+                     position: 'absolute',
+                     top: '64rem',
+                  }}
+               >
                   <img
-                     src="../../src/assets/FirstSeason/bnr_second.jpg"
+                     src="../../src/assets/FirstSeason/img_main-logo.png"
                      alt="Logo de la empresa"
                      style={{
                         width: '100%',
@@ -76,19 +106,22 @@ export const Main = () => {
                         display: 'block',
                      }}
                   />
-               </a>
+               </Box>
             </Box>
-            {/* POP UP LOGO DIAGONAL  */}
+            {/* BLUE RAY AVALIBLE POP UP  */}
             <Box
                sx={{
-                  maxWidth: '80.2rem',
                   position: 'absolute',
-                  top: '64rem',
+                  left: '50%',
+                  bottom: '-134px',
+                  width: '69rem',
+                  zIndex: '5',
+                  transform: 'translateX(-50%)',
                }}
             >
                <img
-                  src="../../src/assets/FirstSeason/img_main-logo.png"
-                  alt="Logo de la empresa"
+                  src="../../src/assets/FirstSeason/text_onair.png"
+                  alt="On Air"
                   style={{
                      width: '100%',
                      maxWidth: '100%',
@@ -98,27 +131,7 @@ export const Main = () => {
                />
             </Box>
          </Box>
-         <Box
-            sx={{
-               position: 'absolute',
-               left: '50%',
-               bottom: '53.4rem',
-               width: '69rem',
-               zIndex: '10',
-               transform: 'translateX(-50%)',
-            }}
-         >
-            <img
-               src="../../src/assets/FirstSeason/text_onair.png"
-               alt="On Air"
-               style={{
-                  width: '100%',
-                  maxWidth: '100%',
-                  height: 'auto',
-                  display: 'block',
-               }}
-            />
-         </Box>
+         {/* NEWS PREVIEW SECTION AREA */}
          <Box
             component="article"
             sx={{
@@ -138,6 +151,8 @@ export const Main = () => {
                <NewsPreview />
             </Box>
          </Box>
+         {/* INTRODUCTION ARTICLE  */}
+         <Introduction />
       </Box>
    )
 }
